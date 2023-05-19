dcc71104c9c5b2a82c18b6ebffc4dcdd26cd3781
Julia <juliavolk96@gmail.com>
2023-05-19 16:34:36 +0400

    Домашнее задание №4 ч1

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,21 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+/*Во время игры вам необходимо будет 
+отображать полоску жизни над игровым персонажем. 
+Для сигнализации пользователю вы решили ввести 
+цветовую индикацию:
+Здоровье более 50 - зелёный;
+Здоровье от 50 и до 15 - жёлтый;
+Менее 15 - красный.*/
+export function getHealthStatus(character) {
+  if(character.health > 50) {
+    return 'healthy';
+  } else if(character.health >= 15) {
+    return 'wounded';
+  } else {
+    return 'critical'
+  }
+}
+
